c8ff92ab4637d833a9c82551c4916592b52f063b
Menno van Grinsven <scanno71@gmail.com>
2019-08-29 21:03:00 +0200

    Z01R: Add DeviceParts
    These DeviceParts are based on the settings originally made for OP3(T)/OP5
    
    Change-Id: Id8d9a02c783ed6008f73c99a2e7754ebe6aed162

---
+++ DeviceParts/src/org/omnirom/device/Startup.java
@@ -0,0 +1,161 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class Startup extends BroadcastReceiver {
+    private static final String TAG = "Startup";
+
+    private static void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, enabled ? "1" : "0");
+    }
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+        restoreAfterUserSwitch(context);
+    }
+
+    public static void restoreAfterUserSwitch(Context context) {
+        Log.e(TAG, "restoreAfterUserSwitch called");
+        // C Gesture
+        String mapping = GestureSettings.DEVICE_GESTURE_MAPPING_0;
+        String value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.MUSIC_PLAY_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        boolean enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_C_APP), enabled);
+
+        // E Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_1;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.CAMERA_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_E_APP), enabled);
+
+        // L Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_2;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.CAMERA_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_L_APP), enabled);
+
+        // M Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_3;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.CAMERA_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_M_APP), enabled);
+
+        // O Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_4;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.CAMERA_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_O_APP), enabled);
+
+        // S Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_5;
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.TORCH_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        enabled = !TextUtils.isEmpty(value) && !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_S_APP), enabled);
+
+        // V Geture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_6;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.TORCH_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_V_APP), enabled);
+
+        // W Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_7;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.MUSIC_PREV_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_W_APP), enabled);
+
+        // Z Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_8;
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.MUSIC_NEXT_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        enabled = !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_Z_APP), enabled);
+
+        // UP Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_9;
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.TORCH_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        enabled = !TextUtils.isEmpty(value) && !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_UP_APP), enabled);
+
+        // DOWN Gesture
+        mapping = GestureSettings.DEVICE_GESTURE_MAPPING_10;
+        if (TextUtils.isEmpty(value)) {
+            value = AppSelectListPreference.TORCH_ENTRY;
+            Settings.System.putString(context.getContentResolver(), mapping, value);
+        }
+        value = Settings.System.getString(context.getContentResolver(), mapping);
+        enabled = !TextUtils.isEmpty(value) && !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+        restore(GestureSettings.getGestureFile(KEY_DOWN_APP), enabled);
+
+        // fp down swipe
+//        value = Settings.System.getString(context.getContentResolver(), GestureSettings.DEVICE_GESTURE_MAPPING_10);
+//        enabled = !TextUtils.isEmpty(value) && !value.equals(AppSelectListPreference.DISABLED_ENTRY);
+//        restore(GestureSettings.FP_GESTURE_LONG_PRESS_APP, enabled);
+    }
+}
