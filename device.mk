a74bf5270bff3fa16d4cc342cc18f7d04bddd65f
kukku14 <gthunjath14@gmail.com>
2020-08-11 15:34:01 +0530

    Z01R: Remove duplicate definition of telephony-ext
    Change-Id: I01798c86ac17c6519aaa2c8c96129fc87b3135af

--- device.mk
+++ device.mk
@@ -160,13 +160,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
   textclassifier.bundle1
 
-# Telephony
-PRODUCT_PACKAGES += \
-  telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Trust HAL
 #PRODUCT_PACKAGES += \
   lineage.trust@1.0-service
